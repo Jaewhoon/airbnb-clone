7bd7ec6ca6c6c312659c95c0e83d31458b63bb44
Jaewhoon Cho <aerospace105@gmail.com>
2022-06-08 22:46:27 +0900

    raw_id를 이용하여 여러 host중 특정 host를 간단하게 찾는 설정 및 Inline Admin을 사용하여 Room 패널에서 사진들을 간단하게 추가하는 방법

--- rooms/admin.py
+++ rooms/admin.py
@@ -17,10 +17,17 @@ class ItemAdmin(admin.ModelAdmin):
         return obj.rooms.count()
 
 
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
+
+    inlines = (PhotoInline,)
 
     fieldsets = (
         (
@@ -87,12 +94,6 @@ class RoomAdmin(admin.ModelAdmin):
         "total_rating",
     )
 
-    # ordering = (
-    #     "name",
-    #     "price",
-    #     "bedrooms",
-    # )
-
     list_filter = (
         "instant_book",
         "host__superhost",
@@ -104,6 +105,8 @@ class RoomAdmin(admin.ModelAdmin):
         "city",
         "country",
     )
+
+    raw_id_fields = ("host",)
 
     search_fields = (
         "=city",
