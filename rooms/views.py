ebc92b3af61def23f3e4f9cd1f77a66314b3670f
Jaewhoon Cho <aerospace105@gmail.com>
2022-06-11 18:48:52 +0900

    장고 템플릿을 활용한 페이지 구분

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,24 @@
+import math
 from django.shortcuts import render
 from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    page_count = math.ceil(models.Room.objects.count() / page_size)
+    page_range = range(1, page_count + 1)
     return render(
         request,
         "rooms/all_rooms.html",
-        context={
+        {
             "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "range": page_range,
         },
     )
