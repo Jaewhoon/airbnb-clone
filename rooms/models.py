01120f10a05488e6639fb86e3d4ccc1a3290a4fb
Jaewhoon Cho <aerospace105@gmail.com>
2022-06-12 01:04:51 +0900

    각 방의 상세 설정들 링크 생성 및 내용 표시

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
@@ -95,9 +96,11 @@ class Room(core_models.TimeStampedModel):
         return self.name
 
     def save(self, *args, **kwargs):
-        print(self.city)
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
 
     def total_rating(self):
         all_reviews = self.reviews.all()
--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
 from django.utils import timezone
 from django.views.generic import ListView
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from . import models
 
 
@@ -17,3 +19,11 @@ class HomeView(ListView):
         now = timezone.now()
         context["now"] = now
         return context
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
