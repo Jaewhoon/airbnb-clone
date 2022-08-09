4adac607fbaabd3f91052780bd187cef3627a9a0
Jaewhoon Cho <aerospace105@gmail.com>
2022-08-09 23:18:11 +0900

    Fix kakao login settings and configure aws elastic beanstalk

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 import os
 import requests
+from django.utils import translation
+from django.http import HttpResponse
 from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import FormView, DetailView, UpdateView
-from django.shortcuts import redirect
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.files.base import ContentFile
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from . import forms, models, mixins
@@ -15,7 +18,6 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = "users/login.html"
     form_class = forms.LoginForm
-    success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -34,8 +36,8 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
 
 def log_out(request):
-    logout(request)
     messages.info(request, f"See you later")
+    logout(request)
     return redirect(reverse("core:home"))
 
 
@@ -62,9 +64,9 @@ def complete_verification(request, key):
         user.email_verified = True
         user.email_secret = ""
         user.save()
-        # To Do : Add success message
+        # to do: add succes message
     except models.User.DoesNotExist:
-        # To Do : Add error message
+        # to do: add error message
         pass
     return redirect(reverse("core:home"))
 
@@ -110,17 +112,11 @@ def github_callback(request):
                     name = profile_json.get("name")
                     email = profile_json.get("email")
                     bio = profile_json.get("bio")
-                    if name is None:
-                        name = username
-                    if email is None:
-                        email = name
-                    if bio is None:
-                        bio = ""
                     try:
                         user = models.User.objects.get(email=email)
-                        if user.login_choices != models.User.LOGIN_GITHUB:
+                        if user.login_method != models.User.LOGIN_GITHUB:
                             raise GithubException(
-                                f"Please log in with: {user.login_choices}"
+                                f"Please log in with: {user.login_method}"
                             )
                     except models.User.DoesNotExist:
                         user = models.User.objects.create(
@@ -128,7 +124,8 @@ def github_callback(request):
                             first_name=name,
                             username=email,
                             bio=bio,
-                            login_choices=models.User.LOGIN_GITHUB,
+                            login_method=models.User.LOGIN_GITHUB,
+                            email_verified=True,
                         )
                         user.set_unusable_password()
                         user.save()
@@ -140,6 +137,71 @@ def github_callback(request):
         else:
             raise GithubException("Can't get code")
     except GithubException as e:
+        messages.error(request, e)
+        return redirect(reverse("users:login"))
+
+
+def kakao_login(request):
+    client_id = os.environ.get("KAKAO_ID")
+    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+    return redirect(
+        f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=account_email,profile_nickname,profile_image"
+    )
+
+
+class KakaoException(Exception):
+    pass
+
+
+def kakao_callback(request):
+    try:
+        code = request.GET.get("code")
+        client_id = os.environ.get("KAKAO_ID")
+        redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+        token_request = requests.get(
+            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"
+        )
+        token_json = token_request.json()
+        error = token_json.get("error", None)
+        if error is not None:
+            raise KakaoException()
+        access_token = token_json.get("access_token")
+        profile_request = requests.get(
+            "https://kapi.kakao.com/v2/user/me",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+        profile_json = profile_request.json()
+        kakao_account = profile_json.get("kakao_account")
+        email = kakao_account["email"]
+        if email is None:
+            raise KakaoException("Please also give me your email")
+        profile = kakao_account["profile"]
+        nickname = profile["nickname"]
+        profile_image = profile["profile_image_url"]
+
+        try:
+            user = models.User.objects.get(email=email)
+            if user.login_choices != models.User.LOGIN_KAKAO:
+                raise KakaoException()
+        except models.User.DoesNotExist:
+            user = models.User.objects.create(
+                email=email,
+                username=email,
+                first_name=nickname,
+                login_choices=models.User.LOGIN_KAKAO,
+                email_verified=True,
+            )
+            user.set_unusable_password()
+            user.save()
+            if profile_image is not None:
+                photo_request = requests.get(profile_image)
+                user.avatar.save(
+                    f"{nickname}-avatar.jpg", ContentFile(photo_request.content)
+                )
+        messages.success(request, f"Welcome back {user.first_name}")
+        login(request, user)
+        return redirect(reverse("core:home"))
+    except KakaoException as e:
         messages.error(request, e)
         return redirect(reverse("users:login"))
 
@@ -208,3 +270,10 @@ def switch_hosting(request):
     except KeyError:
         request.session["is_hosting"] = True
     return redirect(reverse("core:home"))
+
+
+def switch_language(request):
+    lang = request.GET.get("lang", None)
+    if lang is not None:
+        request.session[translation.LANGUAGE_SESSION_KEY] = lang
+    return HttpResponse(status=200)
